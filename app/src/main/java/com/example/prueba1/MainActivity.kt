3f7ee981bfab7c50ca9c9ac68927cb1034436992
stonxzz <cuentamlgpro@gmail.com>
2024-09-06 07:42:42 -0600

    Se hizo  unos ejemplos de como agregar textos e imagenes desde los recursos y como modificar sus estilos desde el componente

--- app/src/main/java/com/example/prueba1/MainActivity.kt
+++ app/src/main/java/com/example/prueba1/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.prueba1
 
+import android.graphics.Picture
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -21,9 +23,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.prueba1.ui.theme.Prueba1Theme
 
 class MainActivity : ComponentActivity() {
@@ -64,6 +75,8 @@ class MainActivity : ComponentActivity() {
 
                 Greeting(name = "World!")
             }*/
+            customText()
+            picture()
         }
     }
 }
@@ -95,7 +108,6 @@ Text(text = "Hello world")
     }
 }
 
-@Preview(showBackground = true)
 @Composable
 fun ModifierExample2(){
     Column(
@@ -112,7 +124,6 @@ fun clickAction(){
     println("Column Clicked")
 }
 
-@Preview(showBackground = true)
 @Composable
 fun ModifierExample3(){
     Column(
@@ -132,4 +143,42 @@ fun ModifierExample3(){
         Text(text = "item 5")
     }
 
+}
+
+@Preview(showBackground = true)
+@Composable
+fun customText(){
+    Column {
+        Text(
+            stringResource(R.string.hello_world_text),
+            color = colorResource(R.color.purple_700),
+            fontSize = 28.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.ExtraBold
+        )
+        val gradientColors = listOf(Color.Cyan, Color.Blue, Color.Red)
+        Text(
+            stringResource(R.string.hello_world_text),
+            style = TextStyle(brush = Brush.linearGradient(colors = gradientColors))
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun picture(){
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Black)
+
+    ){
+        Image(
+            modifier = Modifier
+                .fillMaxWidth(),
+            painter = painterResource(R.drawable.android_logo),
+            contentDescription = "Logo Android",
+            contentScale = ContentScale.Crop
+        )
+    }
 }
