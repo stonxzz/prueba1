332d33168b02cf45c857ed46f8fc1e3cc1f866b9
stonxzz <cuentamlgpro@gmail.com>
2024-10-09 10:38:55 -0600

    Se importo una clase para obtener las medidas del dispositivo que se esta usando y se creo una funcion que dependiendo las medidas adapte el tamaño de sus componentes

--- app/src/main/java/com/example/prueba1/ui/components/PostCard.kt
+++ app/src/main/java/com/example/prueba1/ui/components/PostCard.kt
@@ -1,9 +1,12 @@
 package com.example.ejemplo1.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -52,7 +55,43 @@ fun PostCard(id:Int, title:String, text:String, image: Painter){
             textAlign = TextAlign.Justify,
             modifier = Modifier.padding(10.dp)
         )
+    }
+}
 
-
+@Composable
+fun PostCardCompact(id:Int, title:String, text:String, image: Painter){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(3.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Black,
+            contentColor = Color.White
+        )
+    ) {
+        Row() {
+            Image(modifier = Modifier
+                .width(80.dp)
+                .height(100.dp)
+                .padding(5.dp),
+                painter = image,
+                contentDescription = "Android Logo",
+                contentScale = ContentScale.FillBounds)
+            Column {
+                Text(
+                    text=title,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(10.dp)
+                )
+                Text(
+                    text = text,
+                    fontSize = 13.sp,
+                    textAlign = TextAlign.Justify,
+                    maxLines = 3,
+                    modifier = Modifier.padding(10.dp)
+                )
+            }
+        }
     }
 }
